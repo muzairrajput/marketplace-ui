947f2ee16b6eeb42480a4adde37f2807451c66d1
adhikarghamal <adhikarghamal@gmail.com>
2023-11-30 21:36:14 -0500

    add product updated

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -13,16 +13,14 @@ const AddProduct = ({handleLoggedInUser}) => {
         
         const handleAddProduct = (e) => {
             e.preventDefault();
-            if (e.target.elements[5].value != e.target.elements[6].value) {
-                return alert('Password and Confirm Password dont match');
-            }
+            
             var registerModel = {
-                Name: e.target.elements[4].value, 
-                Description: e.target.elements[5].value, 
-                Category: e.target.elements[4].value, 
-                Price: e.target.elements[2].value, 
-                StockQuality: e.target.elements[3].value,
-                Vendor_ID: e.target.elements[3].value
+                Name: e.target.elements[0].value, 
+                Description: e.target.elements[2].value, 
+                Category: e.target.elements[1].value, 
+                Price: e.target.elements[3].value, 
+                StockQuality: e.target.elements[4].value,
+                Vendor_ID: e.target.elements[0].value
             };
             axios.post('https://souq-marketplace-api.onrender.com/addproduct', registerModel)
             .then(Response => {
@@ -41,6 +39,40 @@ const AddProduct = ({handleLoggedInUser}) => {
             <br/>
         <div className="container">
                     <div className="row">
+                    <div className="col-lg-5 offset-lg-1 col-md-12 order-1 order-lg-2">
+                            <div className="contact-form-content pt-sm-55 pt-xs-55">
+                                <h3 className="contact-page-title">Add New Product</h3>
+                                <div className="contact-form">
+                                    <form onClick={handleAddProduct}  id="contact-form" action="http://demo.hasthemes.com/limupa-v3/limupa/mail.php" method="post">
+                                        <div className="form-group">
+                                            <label>Product Name <span className="required">*</span></label>
+                                            <input type="text" name="productName" id="productName" required/>
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Category <span className="required">*</span></label>
+                                            <input type="text" name="productCategory" id="productCategory" required/>
+                                        </div>
+                                        <div className="form-group mb-30">
+                                            <label>Product Description <span className="required">*</span></label>
+                                            <textarea name="productDescription" id="productDescription" ></textarea>
+                                        </div>                                        
+                                        <div className="form-group">
+                                            <label>Product Price <span className="required">*</span></label>
+                                            <input type="text" name="productPrice" id="productPrice"/>
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Product Quality <span className="required">*</span></label>
+                                            <input type="text" name="productQuality" id="productQuality"/>
+                                        </div>  
+                                        <div className="form-group">
+                                            <button type="submit" value="submit" id="submit" className="li-btn-3" name="submit">Add New Product</button>
+                                        </div>
+                                    </form>
+                                </div>
+                                <p className="form-messege"></p>
+                            </div>
+                        </div>
+
                         <div className="col-lg-6 col-md-12 order-2 order-lg-1">
                             <div className="contact-page-side-content">
                                 <h3 className="contact-page-title">Contact Us</h3>
@@ -62,39 +94,7 @@ const AddProduct = ({handleLoggedInUser}) => {
                             </div>
                         </div>
                         
-                        <div className="col-lg-5 offset-lg-1 col-md-12 order-1 order-lg-2">
-                            <div className="contact-form-content pt-sm-55 pt-xs-55">
-                                <h3 className="contact-page-title">Add New Product</h3>
-                                <div className="contact-form">
-                                    <form onClick={handleAddProduct}  id="contact-form" action="http://demo.hasthemes.com/limupa-v3/limupa/mail.php" method="post">
-                                        <div className="form-group">
-                                            <label>Product Name <span className="required">*</span></label>
-                                            <input type="text" name="customerName" id="customername" required/>
-                                        </div>
-                                        <div className="form-group">
-                                            <label>Category <span className="required">*</span></label>
-                                            <input type="text" name="customerEmail" id="customerEmail" required/>
-                                        </div>
-                                        <div className="form-group mb-30">
-                                            <label>Product Description <span className="required">*</span></label>
-                                            <textarea name="contactMessage" id="contactMessage" ></textarea>
-                                        </div>                                        
-                                        <div className="form-group">
-                                            <label>Product Price <span className="required">*</span></label>
-                                            <input type="text" name="contactSubject" id="contactSubject"/>
-                                        </div>
-                                        <div className="form-group">
-                                            <label>Product Quality <span className="required">*</span></label>
-                                            <input type="text" name="contactSubject" id="contactSubject"/>
-                                        </div>  
-                                        <div className="form-group">
-                                            <button type="submit" value="submit" id="submit" className="li-btn-3" name="submit">Add New Product</button>
-                                        </div>
-                                    </form>
-                                </div>
-                                <p className="form-messege"></p>
-                            </div>
-                        </div>
+                        
                     </div>
                 </div>
         </div>
